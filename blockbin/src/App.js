559815ae41898da1734735604f613f1f4537ac60
Andrew Hunt <andrewhunt@Andrews-MacBook-Pro-2.local>
2017-10-28 17:34:07 -0700

    address comments

--- blockbin/src/App.js
+++ blockbin/src/App.js
@@ -1,32 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header';
+import InputForm from './components/InputForm';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <a href="https://www.blockbin.io">
-            <h1 className="App-title">Blockbin</h1>
-          </a>
-          <a href="about">
-            <h1 className="App-title App-right">About</h1>
-          </a>
-        </header>
-        <div className="App-pagecontainer">
-          <form>
-            <h3>
-              Paste content below to post to Blockbin:
-            </h3>
-            <textarea 
-              className="App-textarea" 
-              rows={10}
-            />
-            <button className="btn btn-primary">
-              Submit
-            </button>
-          </form>
+      <div>
+        <Header />
+        <div className="app-pagecontainer">
+          <InputForm />
         </div>
       </div>
     );
