988fa2f65bb3cc18793fb899a0d7ea9244d145a2
Andrew Hunt <andrew@hunt.codes>
2018-01-11 00:55:02 -0800

    [inputform] address arnauds comments

--- blockbin/src/components/InputForm.js
+++ blockbin/src/components/InputForm.js
@@ -3,7 +3,8 @@ import Web3 from 'web3';
 
 import '../App.css';
 
-const CONTRACT_ADDRESS = '0xa022cc6ff8895fd07b03c480d46f20c412716fdc';
+// TODO: Put in config
+const CONTRACT_ADDRESS = '0xc49f916164a2d47b8934d5454547cc7429ba0c1e';
 
 class InputForm extends Component {
   constructor(props) {
@@ -17,10 +18,10 @@ class InputForm extends Component {
       const newWeb3 = new Web3(window.web3.currentProvider);
       this.web3 = newWeb3;
     } else {
-      console.log('why aren\'t you using mist or metamask?!');
-      // set the provider you want from Web3.providers
-      this.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+      alert('Please install Mist or MetaMask to use Blockbin');
     }
+
+    // TODO: move contract creation to different file - util? root component?
     // Replace with correct ABI
     const abi = JSON.parse('[{"constant":true,"inputs":[{"name":"hash","type":"bytes32"}],"name":"readCube","outputs":[{"name":"","type":"bytes"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"hash","type":"bytes32"}],"name":"softUndelete","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"hash","type":"bytes32"}],"name":"empty","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"data","type":"bytes"},{"name":"hash","type":"bytes32"}],"name":"dumpCube","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"hash","type":"bytes32"}],"name":"softDelete","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"hash","type":"bytes32"}],"name":"forceEmpty","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"inputs":[],"payable":false,"type":"constructor"}]')
     const BlockbinContract = this.web3.eth.contract(abi);
@@ -40,20 +41,41 @@ class InputForm extends Component {
     event.preventDefault();
     const cubeBytes = this.web3.fromAscii(this.state.pastedText);
     const contentHash = this.web3.sha3(cubeBytes);
+    const contractInstance = this.contractInstance;
+    const web3 = this.web3;
 
-    // metamask uses accounts[0] to pass the preferred account
-    const txId = this.contractInstance.dumpCube.sendTransaction(
+    function submitTx(gasEstimate) {
+      // metamask uses accounts[0] to pass the preferred account
+      contractInstance.dumpCube.sendTransaction(
+        cubeBytes, 
+        contentHash, 
+        {
+          from: web3.eth.accounts[0], 
+          gas: gasEstimate,
+        },
+        function(error, result){
+          if (!error) {
+            alert('Saved!\nYour lookup hash is: ' + contentHash + '\nYour transaction id is: ' + result);
+          } else {
+            console.error(error);
+            alert('Error in processing your transaction');
+          }
+        }
+      );
+    }
+
+    this.contractInstance.dumpCube.estimateGas(
       cubeBytes, 
       contentHash, 
-      {
-        from: this.web3.eth.accounts[0], 
-        gas: 2000000,
-      },
+      { from: this.web3.eth.accounts[0] },
       function(error, result){
-        if(!error)
-          alert('Saved!\nYour lookup hash is: ' + contentHash + '\nYour transaction id is: ' + result);
-        else
-          console.error(error);
+        if (!error) {
+          console.log('gas estimate: ' + result);
+          submitTx(result);
+        } else {
+          console.error('not able to estimate gas. error msg: ' + error);
+          return;
+        }
       }
     );
   }
