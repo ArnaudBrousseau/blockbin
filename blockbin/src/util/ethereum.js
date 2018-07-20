7e57003738c5de3482be5b0e734a20aa78283b61
Arnaud Brousseau <arnaud.brousseau@gmail.com>
2018-07-20 22:43:13 +0800

    Moves CONTRACT_ADDRESS to .env

--- blockbin/src/util/ethereum.js
+++ blockbin/src/util/ethereum.js
@@ -1,6 +1,3 @@
-// TODO: Put in config
-const CONTRACT_ADDRESS = '0xc49f916164a2d47b8934d5454547cc7429ba0c1e';
-
 /**
  * Instantiates the Blockbin contract
  * Note: `process.env` is a bit magic. It's compiled out by Webpack and gets
@@ -10,7 +7,7 @@ const CONTRACT_ADDRESS = '0xc49f916164a2d47b8934d5454547cc7429ba0c1e';
 var createBlockbinContract = function(web3) {
     const abi = JSON.parse(process.env.REACT_APP_BLOCKBIN_ABI);
     const contract = web3.eth.contract(abi);
-    contract.at(CONTRACT_ADDRESS);
+    contract.at(process.env.REACT_APP_CONTRACT_ADDRESS);
 };
 
 module.exports = {
