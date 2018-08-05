c708feb3de2223b0a8e64bd85c8b3243157c3d75
Arnaud Brousseau <arnaud.brousseau@gmail.com>
2018-08-05 17:35:29 +0700

    Implements ShowCubeForm component redirection.

--- webapp/src/components/ShowCubeForm.js
+++ webapp/src/components/ShowCubeForm.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
+import { Redirect } from "react-router-dom";
 
 
 class ShowCubeForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '', cubeURL: undefined };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  isValidSHA3(str) {
+    // TODO: refine this :)
+    return !!str;
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    if (this.isValidSHA3(this.state.value)) {
+      this.setState({
+        value: this.state.value,
+        cubeURL: '/cube/' + this.state.value
+      });
+    }
+  }
+
+  handleChange(e) {
+    this.setState({value: e.target.value});
+  }
+
   render() {
+    if (this.state.cubeURL) {
+      return <Redirect to={this.state.cubeURL} />;
+    }
     return (
-      <form className="header-form">
+      <form className="header-form" onSubmit={this.handleSubmit}>
         <label>
           Cube SHA
-          <input type="text" name="sha" className="header-input" />
+          <input type="text" value={this.state.value} onChange={this.handleChange} name="sha" className="header-input" />
         </label>
         <button className="header-submit-button">Show</button>
       </form>
