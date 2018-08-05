c2f8d1c34bee1cd595a65be4c6443739689b59b4
Arnaud Brousseau <arnaud.brousseau@gmail.com>
2018-08-05 15:50:00 +0700

    Adds Retrieval form to header.

--- webapp/src/components/Header.js
+++ webapp/src/components/Header.js
@@ -6,9 +6,18 @@ class Header extends Component {
   render() {
     return (
       <header className="header">
-        <a href="/" className="header-link">
-          <h1 className="header-title">Blockbin</h1>
-        </a>
+        <div className="header-container">
+          <a href="/" className="header-link">
+            <h1 className="header-title">Blockbin</h1>
+          </a>
+          <form className="header-form">
+            <label>
+              Cube SHA
+              <input type="text" name="sha" className="header-input" />
+            </label>
+            <button className="header-submit-button">Show</button>
+          </form>
+        </div>
       </header>
     );
   }
